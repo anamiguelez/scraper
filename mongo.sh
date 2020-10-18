9a803afa06b21902b5ee33f19cee98edfafe0083
anamiguelez <72510803+anamiguelez@users.noreply.github.com>
2020-10-18 12:34:58 +0200

    Add files via upload

---
+++ mongo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodv-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl status mongod
+cd <mongodb installation dir>/bin
+mongo 
